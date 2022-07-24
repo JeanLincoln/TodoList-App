83efc2c98978b7bc6e4dd911702ead9fcb66b29c
Jean Lincoln <jeanlincol@gmail.com>
2022-07-24 11:19:39 -0700

    DefaultLayout, fixed structure

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -1,14 +1,13 @@
 import { Header } from '../Components/Header'
 import { Outlet } from 'react-router-dom'
-import { HeaderContainer } from './styles'
 
 export function DefaultLayout(){
     return (
         <div>
-          <HeaderContainer>
+          <>
             <Header />
             <Outlet />
-          </HeaderContainer>
+          </>
         </div>
       )
 }
