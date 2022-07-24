3bcd4c9ad47573f6f463e0259668ac2d39073168
Jean Lincoln <jeanlincol@gmail.com>
2022-07-24 09:34:17 -0700

    TodoListApp, Global Theme Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/Global"
 import { DefaultTheme } from "./styles/themes/Default"
 
 export function App() {
   return (
    <ThemeProvider theme={DefaultTheme}>
-    
+    <GlobalStyle />
+    <h1>Hello World</h1>
    </ThemeProvider>
   )
 }
---
+++ src/styles/Global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+
+    html{
+        font-size: 62.5%;
+    }
+
+    :focus{
+        outline:0;
+        box-shadow: 0 0 0 2px ${(props) => props.theme['purple-dark']};
+    }
+
+    body{
+        background:${(props) => props.theme['gray-700']};
+        color:${(props) => props.theme['gray-100']};
+        -webkit-font-smoothing:antialiased;
+    }
+
+    border-style, input, textarea, button {
+        font-family: 'Inter', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+`
