c5400a1420513f9af7b9ba9e0ea430d2d7de7eeb
Jean Lincoln <jeanlincol@gmail.com>
2022-07-24 10:06:52 -0700

    Global styles, background color fixed

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -17,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        background:${(props) => props.theme['gray-700']};
+        background:${(props) => props.theme['gray-600']};
         color:${(props) => props.theme['gray-100']};
         -webkit-font-smoothing:antialiased;
     }
