6c3fab195283e0168dd53923dc595dc6f9585adc
Jean Lincoln <jeanlincol@gmail.com>
2022-07-25 16:22:41 -0700

    Header component, added navegation and its styles

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,5 +1,7 @@
 import * as S from './styles'
 import Logo  from '../../Assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
+import { Clipboard,Check } from 'phosphor-react'
 
 export function Header(){
     return(
@@ -8,6 +10,15 @@ export function Header(){
                 src={Logo} 
                 alt="Site Logo, A rocket and a text 'Todo'" 
             />
+            <S.Nav>
+                <NavLink to="/" title="Home">
+                    <Clipboard size={32}/>
+                </NavLink>
+                <NavLink to="/TasksDones" title="TasksDones">
+                    <Check size={32}/>
+                </NavLink>
+            </S.Nav>
+            
         </S.HeaderContainer>
     )
 }
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -5,9 +5,15 @@ export const HeaderContainer = styled.header`
   align-items: center;
   justify-content: center;
   flex: 1;
+  gap:5rem;
 
   height:20rem;
 
   background-color: ${({theme}) => theme['gray-700']};
+`
 
+export const Nav = styled.nav`
+    display:flex;
+    justify-content:space-between;
+    gap:5rem;
 `
