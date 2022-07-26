e1bf1baff5f5285e4ca485810877334143de454c
Jean Lincoln <jeanlincol@gmail.com>
2022-07-26 12:07:24 -0700

    Task component, delete task, checktask logic added

--- src/Pages/Home/Components/Task/index.tsx
+++ src/Pages/Home/Components/Task/index.tsx
@@ -5,17 +5,28 @@ type TaskProps = {
     id: string,
     taskText:string,
     isChecked:boolean
+    onDeleteTask: (TodoId:string) => void
+    onCheckTask: (TodoId:string) => void
 }
 
-export function Task({id,taskText}:TaskProps){
+export function TaskItem({id,taskText,onDeleteTask,onCheckTask}:TaskProps){
+
+const deleteTask = () => {
+    onDeleteTask(id)
+}
+
+const checkTask = () => {
+    onCheckTask(id)
+}
+
     return(
         <S.Task>
             <div className='taskContent'>
-            <input id={id} type="checkbox" />
+            <input onChange={checkTask} type="checkbox" />
             <span className="checkmark"></span>
             <span>{taskText}</span>
             </div>
-            <Trash size={25}/>
+            <button onClick={deleteTask}><Trash size={25}/></button>
         </S.Task>
         )
 }
